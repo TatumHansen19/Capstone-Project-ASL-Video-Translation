213ff0f9408570967a291a8baa403c95eb09af14
TatumHansen19 <127242799+TatumHansen19@users.noreply.github.com>
2024-09-16 19:31:03 -0700

    Sprint1.py

--- Sprint1.py
+++ Sprint1.py
@@ -1,14 +1,14 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)  #selects camera
 
-while True:
+while True: #continues until the conditions of pressing the 'q' key is met. 
     ret, frame = cap.read()
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
-cap.release()
-cv2.destroyAllWindows()
+cap.release() #releases the camera
+cv2.destroyAllWindows() #closes all windows
